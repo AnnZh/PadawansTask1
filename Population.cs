209b06298d55c75964c7824f78e24614fdd88590
AnnZh <ann.zhurok@gmail.com>
2019-02-13 23:45:55 +0300

    Done

--- Population.cs
+++ Population.cs
@@ -6,8 +6,19 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (initialPopulation < 0 || percent < 0 || visitors < 0 || currentPopulation < 0)
+                throw new ArgumentOutOfRangeException();
+
+            double n1 = initialPopulation;
+            int years = 0;
+
+            while (n1 < currentPopulation)
+            {
+                n1 = n1 + (n1 * percent * 0.01) + visitors;
+                years++;
+
+            }
+            return years;
         }
     }
 }
